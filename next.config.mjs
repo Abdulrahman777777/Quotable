ba24895099a7d95034fd2035d2769286d64d96ac
Abdulrahman777777 <boodyisthebest@gmail.com>
2024-08-13 15:26:18 +0300

    fixing some stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: { API_QUOTE: process.env.API_QUOTE },
+  env: { API_QUOTE: process.env.API_QUOTE, API_IMAGE: process.env.API_IMAGE },
 };
 
 export default nextConfig;
